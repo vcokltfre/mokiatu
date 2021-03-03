4eb8663afefe2237a88e1106478aadefb3e84a2d
vcokltfre <vcokltfre@gmail.com>
2021-03-03 19:42:12 +0000

    feat: add support for more hashes

--- src/cogs/hashes.py
+++ src/cogs/hashes.py
@@ -1,6 +1,6 @@
 from discord.ext import commands
 from discord import Embed
-from hashlib import sha256, sha512, md5
+from hashlib import sha256, sha512, md5, new, algorithms_available
 
 from src.internal import Bot
 
@@ -42,6 +42,17 @@ class Hashes(commands.Cog):
 
         await ctx.send(embed=self.build_embed("MD5", text, output, "MD5 is an insecure hash algorithm with known vulnerabilities and collisions."))
 
+    @commands.command(name="hash")
+    async def hash_sha512(self, ctx: commands.Context, name: str = None, *, text: str = None):
+        """Hash text using a user provided hash."""
+        try:
+            h = new(name, text.encode("utf-8"))
+        except Exception as e:
+            hashes_help = "Available hashes: " + ", ".join(sorted(algorithms_available))
+            return await ctx.send("No hashing algorithm found by that name!\n" + hashes_help)
+
+        await ctx.send(embed=self.build_embed(name, text, h))
+
     @commands.command(name="hashes")
     async def hashes(self, ctx: commands.Context):
         """Get a list of hashes supported by Mokiatu."""
@@ -50,6 +61,8 @@ class Hashes(commands.Cog):
         for command in self.walk_commands():
             if command.name == "hashes": continue
             embed.add_field(name=command.name, value=command.help, inline=False)
+
+        embed.set_footer(text="Use .hash <hash> for more specific hashes like ripemd160")
 
         await ctx.send(embed=embed)
 
